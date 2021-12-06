503cd35c44dc4a5a8456ebf0861ca7276d3e1d4d
Emily Roberts <nopeinomicon@posteo.net>
2021-12-05 19:11:57 -0700

    Remove dmenu funcs

--- utils/dmenu/dmenu-misc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-getmenu() {
-    if command -v bemenu >/dev/null; then
-        printf "bemenu"
-    elif command -v bemenu >/dev/null; then
-        printf "dmenu"
-    fi
-}
--- utils/dmenu/runmenu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ksh
-runmenu() {
-    selection=$(echo "$(getapps)$(getfunctionnames)" | sort -u | eval "$(getmenu)")
-    $selection
-}
