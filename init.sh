f0451026d3a7db8d168729d0e857799c38dc4d0b
Emily Roberts <nopeinomicon@posteo.net>
2021-12-05 19:03:04 -0700

    Update init script and dependencies file

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-scriptdir=$(dirname "$(realpath "$0")");
+scriptdir=$(dirname "$(realpath "init.sh")");
 . $scriptdir/image/screenshot.sh;
 . $scriptdir/utils/shellutils.sh;
 . $scriptdir/utils/pathutils.sh;
 . $scriptdir/utils/getapps.sh;
 . $scriptdir/utils/function/embedfunction.sh;
+. $scriptdir/utils/dmenu/dmenu-misc.sh;
+. $scriptdir/utils/dmenu/runmenu.sh;
