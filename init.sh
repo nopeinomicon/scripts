66080d468c3f71a1f0fae294abc02decfeb043bb
Emily Roberts <nopeinomicon@posteo.net>
2021-12-05 19:17:25 -0700

    Remove dmenu scripts from init

--- init.sh
+++ init.sh
@@ -5,5 +5,3 @@ scriptdir=$(dirname "$(realpath "init.sh")");
 . $scriptdir/utils/pathutils.sh;
 . $scriptdir/utils/getapps.sh;
 . $scriptdir/utils/function/embedfunction.sh;
-. $scriptdir/utils/dmenu/dmenu-misc.sh;
-. $scriptdir/utils/dmenu/runmenu.sh;
